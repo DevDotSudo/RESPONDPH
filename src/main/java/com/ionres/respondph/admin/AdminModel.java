cd23f1511724b3c96f273ea7ea62334efe71fa9e
DevDotSudo <daviele17@gmail.com>
2025-12-05 15:10:48 +0800

    RespondPH December 5, 2025 UI Update

--- src/main/java/com/ionres/respondph/admin/AdminModel.java
+++ src/main/java/com/ionres/respondph/admin/AdminModel.java
@@ -1,5 +1,26 @@
 package com.ionres.respondph.admin;
 
 public class AdminModel {
-    
+    private String adminID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String password;
+
+    public AdminModel(String adminID, String firstName, String middleName, String lastName, String password) {
+        this.adminID = adminID;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String getAdminID() {
+        return adminID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
 }
